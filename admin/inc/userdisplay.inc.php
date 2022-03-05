1969cab873e77e780fd1261fa263b4fb2b6ddc52
Your NameOlolosha <ololosha@ololo.com>
2022-03-05 14:49:11 +0300

    fixUserdisplay

--- admin/inc/userdisplay.inc.php
+++ admin/inc/userdisplay.inc.php
@@ -2,7 +2,7 @@
     <div class="userdisplay__title">ПОЛЬЗОВАТЕЛЬ:</div>
     <div class="userdisplay__name"><?= $_SESSION['editableUserName'] ?></div>
     <div class="userdisplay__func">
-        <a href="<?= $_SERVER['REQUEST_URI'] ?>?popuptask=changeuser&uid=<?= $item['id'] ?>&ref=<?= $_SERVER['REQUEST_URI'] ?>" class="userdisplay__button">Изменить</a>
+        <a href="<?= $_SERVER['REQUEST_URI'] ?>?popuptask=changeuser&uid=<?= $_SESSION['editableUserID'] ?>&ref=<?= $_SERVER['REQUEST_URI'] ?>" class="userdisplay__button">Изменить</a>
 
         <?php
         if (deletedUser($_SESSION['editableUserID']) == 1) {
